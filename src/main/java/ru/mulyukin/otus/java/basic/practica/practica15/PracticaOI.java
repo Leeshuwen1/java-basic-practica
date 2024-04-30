6dccb054842921100be271551e55a09692f4d70c
Alexey Mulyukin <adiott52@gmail.com>
2024-04-30 15:41:17 +0300

    Practica15

--- src/main/java/ru/mulyukin/otus/java/basic/practica/practica15/PracticaOI.java
+++ src/main/java/ru/mulyukin/otus/java/basic/practica/practica15/PracticaOI.java
@@ -1,20 +1,41 @@
 package ru.mulyukin.otus.java.basic.practica.practica15;
 
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.nio.charset.StandardCharsets;
 
 public class PracticaOI {
     public static void main(String[] args) {
 
-        try (FileReader fileReader = new FileReader("1.txt", StandardCharsets.UTF_8)) {
-            int byteRead;
-            while ((byteRead = fileReader.read()) != -1) {
-                System.out.print((char) byteRead);
+        try(FileOutputStream outputStream = new FileOutputStream("2.txt")){
+            String str = "hello world";
+            for(char ch: str.toCharArray()){
+                outputStream.write(ch);
             }
-        } catch (IOException e) {
+
+        }catch (IOException e){
             System.out.println("Error " + e.getMessage());
         }
+
+
+//        try (FileInputStream fileInputStream = new FileInputStream("1.txt")) {
+//            byte[] buffer = new byte[1024];
+//            int len;
+//            while ((len = fileInputStream.read(buffer)) != -1) {
+//                String data = new String(buffer, 0, len, StandardCharsets.UTF_8);
+//                System.out.println(data);
+//            }
+//        } catch (IOException e) {
+//            System.out.println("Error " + e.getMessage());
+//        }
+
+//        try (FileReader fileReader = new FileReader("1.txt", StandardCharsets.UTF_8)) {
+//            int byteRead;
+//            while ((byteRead = fileReader.read()) != -1) {
+//                System.out.print((char) byteRead);
+//            }
+//        } catch (IOException e) {
+//            System.out.println("Error " + e.getMessage());
+//        }
 
 
     }
