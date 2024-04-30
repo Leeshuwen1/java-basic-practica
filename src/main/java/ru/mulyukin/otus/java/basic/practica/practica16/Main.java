56713d3afc36083f7937ae3fb750a096d4c09283
Alexey Mulyukin <adiott52@gmail.com>
2024-04-30 15:44:11 +0300

    Practica16

---
+++ src/main/java/ru/mulyukin/otus/java/basic/practica/practica16/Main.java
@@ -0,0 +1,44 @@
+package ru.mulyukin.otus.java.basic.practica.practica16;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) {
+
+        try (FileOutputStream outputStream = new FileOutputStream("2.txt")) {
+            String str = "hello world";
+            for (char ch : str.toCharArray()) {
+                outputStream.write(ch);
+            }
+
+        } catch (IOException e) {
+            System.out.println("Error " + e.getMessage());
+        }
+
+
+//        try (FileInputStream fileInputStream = new FileInputStream("1.txt")) {
+//            byte[] buffer = new byte[1024];
+//            int len;
+//            while ((len = fileInputStream.read(buffer)) != -1) {
+//                String data = new String(buffer, 0, len, StandardCharsets.UTF_8);
+//                System.out.println(data);
+//            }
+//        } catch (IOException e) {
+//            System.out.println("Error " + e.getMessage());
+//        }
+
+//        try (FileReader fileReader = new FileReader("1.txt", StandardCharsets.UTF_8)) {
+//            int byteRead;
+//            while ((byteRead = fileReader.read()) != -1) {
+//                System.out.print((char) byteRead);
+//            }
+//        } catch (IOException e) {
+//            System.out.println("Error " + e.getMessage());
+//        }
+
+
+    }
+}
+
+
